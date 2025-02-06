dfbbdf6b90b70d9cadef5d422cf73eac6a659cb5
zjardyn <zlieraho@uwaterloo.ca>
2025-02-06 12:39:00 -0500

    added more settings

--- lua/user/settings.lua
+++ lua/user/settings.lua
@@ -1,19 +1,88 @@
-vim.o.signcolumn = "no"
-vim.o.incsearch = true
-vim.o.number = true
-vim.o.relativenumber = true
-vim.o.wrap = false
-vim.o.swapfile = false
-vim.o.backup = false
-vim.o.undodir = os.getenv("HOME") .. "/.vim/undodir"
-vim.o.autochdir = true
+vim.opt.signcolumn = "no"        -- Disable the sign column (no space for git signs, diagnostics, etc.)
+vim.opt.incsearch = true         -- Enable incremental search (highlights matches as you type)
+vim.opt.number = true            -- Show absolute line numbers
+vim.opt.relativenumber = true    -- Show relative line numbers for easy navigation
+vim.opt.wrap = false             -- Disable line wrapping
+vim.opt.swapfile = false         -- Disable swap file creation
+vim.opt.backup = false           -- Disable backup files
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"  -- Set undo history directory
+vim.opt.autochdir = true         -- Automatically change the working directory to the current file
 
-vim.opt.tabstop = 4       -- The width of a tab character
-vim.opt.shiftwidth = 4    -- The width for auto-indents
-vim.opt.expandtab = true  -- Convert tabs to spaces
+-- use less indentation
+local tabsize = 2
+vim.opt.expandtab = true
+vim.opt.shiftwidth = tabsize
+vim.opt.tabstop = tabsize
 
-vim.g.rout_follow_colorscheme = true
+vim.g.rout_follow_colorscheme = true  -- Make R console output follow the current colorscheme
 
+-- proper colors
+vim.opt.termguicolors = true
+
+-- show insert mode in terminal buffers
+vim.api.nvim_set_hl(0, 'TermCursor', { fg = '#A6E3A1', bg = '#A6E3A1' })
+
+-- disable fill chars (the ~ after the buffer)
+vim.o.fillchars = 'eob: '
+
+-- more opinionated
+vim.opt.number = true -- show linenumbers
+vim.opt.mouse = 'a' -- enable mouse
+vim.opt.mousefocus = true
+vim.opt.clipboard:append 'unnamedplus' -- use system clipboard
+
+vim.opt.timeoutlen = 400 -- until which-key pops up
+vim.opt.updatetime = 250 -- for autocommands and hovers
+
+-- don't ask about existing swap files
+vim.opt.shortmess:append 'A'
+
+-- mode is already in statusline
+vim.opt.showmode = false
+
+-- smarter search
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- indent
+vim.opt.smartindent = true
+vim.opt.breakindent = true
+
+vim.opt.list = false -- Don't show special characters
+
+-- consistent number column
+vim.opt.signcolumn = 'yes:1'
+
+-- how to show autocomplete menu
+vim.opt.completeopt = 'menuone,noinsert'
+
+-- global statusline
+vim.opt.laststatus = 3
+
+-- hide cmdline when not used
+vim.opt.cmdheight = 1
+
+-- split right and below by default
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+--tabline
+vim.opt.showtabline = 1
+
+--windowline
+vim.opt.winbar = '%f'
+
+-- don't continue comments automagically
+-- https://neovim.io/doc/user/options.html#'formatoptions'
+vim.opt.formatoptions:remove 'c'
+vim.opt.formatoptions:remove 'r'
+vim.opt.formatoptions:remove 'o'
+
+-- scroll before end of window
+vim.opt.scrolloff = 5
+
+-- (don't == 0) replace certain elements with prettier ones
+vim.opt.conceallevel = 0
 
 -- TODO: Add White colourscheme
 -- TODO: Add markdown support
